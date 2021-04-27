c13d8fd31bee0db3ea68a680322bf4ce01256e5a
Sakda Suwantheerangkoon <sakdakluy@gmail.com>
2021-04-28 00:42:31 +0700

    mail ui fix

--- app/src/main/java/tech/parzival48/thoeic/ui/popup/AboutDialogFragment.kt
+++ app/src/main/java/tech/parzival48/thoeic/ui/popup/AboutDialogFragment.kt
@@ -1,11 +1,15 @@
 package tech.parzival48.thoeic.ui.popup
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import tech.parzival48.thoeic.databinding.FragmentAboutDialogBinding
+import timber.log.Timber
 
 class AboutDialogFragment : DialogFragment() {
 
@@ -26,7 +30,21 @@ class AboutDialogFragment : DialogFragment() {
 
 		binding.also {
 			it.btnClose.setOnClickListener { dismiss() }
+			it.btnEmail.setOnClickListener(OnEmailClickListener())
 		}
+	}
+
+	private inner class OnEmailClickListener : View.OnClickListener {
+		override fun onClick(v: View?) {
+			val intent = Intent(Intent.ACTION_SEND)
+			intent.data = Uri.parse("mailto:parzival48.tech@gmail.com")
+			try {
+			    startActivity(intent)
+			} catch(e: ActivityNotFoundException) {
+				Timber.d("ERROR! Activity no found.")
+			}
+		}
+
 	}
 
 }
